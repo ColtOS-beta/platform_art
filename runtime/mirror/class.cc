580b596d2d4650f6616d0a2e35936b277a30e891
Vladimir Marko <vmarko@google.com>
2022-09-19 17:18:24 +0000

    ART: Add static helpers for ArtMethod's access flags.
    Add static helper methods similar to the existing function
    `ArtMethod::IsDirect()` for other functions querying access
    flags. Use these helpers to avoid some loads that clang++
    does not eliminate because the access flags field is atomic.
    
    Also fix some comments.
    
    Test: m test-art-host-gtest
    Test: testruner.py --host --optimizing
    Change-Id: I3d8dfabd2c9fca37fbf5dd98e4b61d7ce488f717
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- runtime/mirror/class.cc
+++ runtime/mirror/class.cc
@@ -1432,7 +1432,7 @@ ArtField* Class::FindStaticField(ObjPtr<mirror::DexCache> dex_cache, uint32_t fi
 void Class::ClearSkipAccessChecksFlagOnAllMethods(PointerSize pointer_size) {
   DCHECK(IsVerified());
   for (auto& m : GetMethods(pointer_size)) {
-    if (!m.IsNative() && m.IsInvokable()) {
+    if (m.IsManagedAndInvokable()) {
       m.ClearSkipAccessChecks();
     }
   }
@@ -1441,7 +1441,7 @@ void Class::ClearSkipAccessChecksFlagOnAllMethods(PointerSize pointer_size) {
 void Class::ClearMustCountLocksFlagOnAllMethods(PointerSize pointer_size) {
   DCHECK(IsVerified());
   for (auto& m : GetMethods(pointer_size)) {
-    if (!m.IsNative() && m.IsInvokable()) {
+    if (m.IsManagedAndInvokable()) {
       m.ClearMustCountLocks();
     }
   }
@@ -1450,7 +1450,7 @@ void Class::ClearMustCountLocksFlagOnAllMethods(PointerSize pointer_size) {
 void Class::ClearDontCompileFlagOnAllMethods(PointerSize pointer_size) {
   DCHECK(IsVerified());
   for (auto& m : GetMethods(pointer_size)) {
-    if (!m.IsNative() && m.IsInvokable()) {
+    if (m.IsManagedAndInvokable()) {
       m.ClearDontCompile();
     }
   }
@@ -1459,7 +1459,7 @@ void Class::ClearDontCompileFlagOnAllMethods(PointerSize pointer_size) {
 void Class::SetSkipAccessChecksFlagOnAllMethods(PointerSize pointer_size) {
   DCHECK(IsVerified());
   for (auto& m : GetMethods(pointer_size)) {
-    if (!m.IsNative() && m.IsInvokable()) {
+    if (m.IsManagedAndInvokable()) {
       m.SetSkipAccessChecks();
     }
   }
