hopez13/art

Reject boot images if there is a read barrier state mismatch.
This check is done for apps
(https://cs.android.com/android/platform/superproject/+/master:art/runtime/oat_file_assistant.cc;l=540;drc=b7a86cb474c180bcb93dfefb420bee00c0b70103).
It should be done for boot images as well.

Bug: 242553398
Test: Presubmit
Change-Id: I1fa5a4efe204611c8af58d006c7a04c002b6c184

runtime/gc/space/image_space.cc
@@ -51,6 +51,7 @@
 #include "dex/art_dex_file_loader.h"
 #include "dex/dex_file_loader.h"
 #include "exec_utils.h"
+#include "fmt/format.h"
 #include "gc/accounting/space_bitmap-inl.h"
 #include "gc/task_processor.h"
 #include "image-inl.h"
@@ -71,13 +72,19 @@ namespace art {
 namespace gc {
 namespace space {
 
-using android::base::Join;
-using android::base::StringAppendF;
-using android::base::StringPrintf;
+namespace {
+
+using ::android::base::Join;
+using ::android::base::StringAppendF;
+using ::android::base::StringPrintf;
+
+using ::fmt::literals::operator""_format;  // NOLINT
 
 // We do not allow the boot image and extensions to take more than 1GiB. They are
 // supposed to be much smaller and allocating more that this would likely fail anyway.
 static constexpr size_t kMaxTotalImageReservationSize = 1 * GB;
+
+}  // namespace
 
 Atomic<uint32_t> ImageSpace::bitmap_index_(0);
 
@@ -3358,6 +3365,15 @@ bool ImageSpace::ValidateOatFile(const OatFile& oat_file,
                                  ArrayRef<const int> dex_fds,
                                  const std::string& apex_versions) {
   if (!ValidateApexVersions(oat_file, apex_versions, error_msg)) {
+    return false;
+  }
+
+  // For a boot image, the key value store only exists in the first OAT file. Skip other OAT files.
+  if (oat_file.GetOatHeader().GetKeyValueStoreSize() != 0 &&
+      oat_file.GetOatHeader().IsConcurrentCopying() != gUseReadBarrier) {
+    *error_msg =
+        "ValidateOatFile found read barrier state mismatch (oat file: {}, runtime: {})"_format(
+            oat_file.GetOatHeader().IsConcurrentCopying(), gUseReadBarrier);
     return false;
   }
 

ColtOS-beta/platform_art

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

runtime/gc/space/image_space.cc
@@ -49,6 +49,7 @@
 #include "dex/art_dex_file_loader.h"
 #include "dex/dex_file_loader.h"
 #include "exec_utils.h"
+#include "fmt/format.h"
 #include "gc/accounting/space_bitmap-inl.h"
 #include "gc/task_processor.h"
 #include "image-inl.h"
@@ -69,13 +70,19 @@ namespace art {
 namespace gc {
 namespace space {
 
-using android::base::Join;
-using android::base::StringAppendF;
-using android::base::StringPrintf;
+namespace {
+
+using ::android::base::Join;
+using ::android::base::StringAppendF;
+using ::android::base::StringPrintf;
+
+using ::fmt::literals::operator""_format;  // NOLINT
 
 // We do not allow the boot image and extensions to take more than 1GiB. They are
 // supposed to be much smaller and allocating more that this would likely fail anyway.
 static constexpr size_t kMaxTotalImageReservationSize = 1 * GB;
+
+}  // namespace
 
 Atomic<uint32_t> ImageSpace::bitmap_index_(0);
 
@@ -3578,6 +3585,15 @@ bool ImageSpace::ValidateOatFile(const OatFile& oat_file,
                                  ArrayRef<const std::string> dex_filenames,
                                  ArrayRef<const int> dex_fds) {
   if (!ValidateApexVersions(oat_file, error_msg)) {
+    return false;
+  }
+
+  // For a boot image, the key value store only exists in the first OAT file. Skip other OAT files.
+  if (oat_file.GetOatHeader().GetKeyValueStoreSize() != 0 &&
+      oat_file.GetOatHeader().IsConcurrentCopying() != kUseReadBarrier) {
+    *error_msg =
+        "ValidateOatFile found read barrier state mismatch (oat file: {}, runtime: {})"_format(
+            oat_file.GetOatHeader().IsConcurrentCopying(), kUseReadBarrier);
     return false;
   }
 